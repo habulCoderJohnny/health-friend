bf701679637f880f0554aec4baac9c629d5c270d
habulCoderJohnny <fokruljohnny@gmail.com>
2022-09-12 18:20:42 +0600

    Projet intial Setup

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './component/Main';
+
+const Home = () => {
+    return (
+        <div>
+            <Main></Main>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/component/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <h1>hello health-bondhu</h1>
+        </div>
+    );
+};
+
+export default Banner;
