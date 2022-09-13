f1658afcf9be744c419dc7bcde3192a4599dc0b2
habulCoderJohnny <fokruljohnny@gmail.com>
2022-09-13 11:02:12 +0600

    project almost there

--- src/component/Main.js
+++ src/component/Main.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Appointment from './Appointment';
 import Banner from './Banner/Banner';
 import FAQ from './FAQ';
+import Footer from './Footer';
 import Navbar from './Navbar';
 import Exprience from './Services/Exprience';
 import Services from './Services/Services';
+import Testimonial from './Testimonial';
 
 const Main = () => {
     return (
@@ -15,6 +17,8 @@ const Main = () => {
             <Exprience></Exprience>
             <Appointment></Appointment>
             <FAQ></FAQ>
+            <Testimonial></Testimonial>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/component/Testimonial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Testimonial = () => {
+    return (
+        <div>
+                        <h1 className='text-secondary text-center'>Testimonial</h1>
+            <h1 className='text-center text-2xl font-bold'>What they say?</h1>
+        </div>
+    );
+};
+
+export default Testimonial;
